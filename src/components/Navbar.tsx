863071f905719c4d01de189a377d919c82a95354
Sam <samennis127@gmail.com>
2023-02-02 04:54:54 +0000

    Fix mobile navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,29 @@
-import { Fragment } from 'react'
-import { Popover, Transition } from '@headlessui/react'
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
 import {
+  AdjustmentsHorizontalIcon,
   Bars3Icon,
-} from '@heroicons/react/24/outline'
-import Link from 'next/link'
-import { useSession, signIn, signOut } from 'next-auth/react'
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+import { CurrencyDollarIcon } from "@heroicons/react/20/solid";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Navbar() {
-  const {data: session} = useSession();
+  const { data: session } = useSession();
 
   return (
-    <Popover className="relative bg-purple-200">
+    <Popover className="relative bg-white">
       <div className="mx-auto max-w-7xl px-6">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-              <Link href="/">
+            <a href="#">
               <span className="sr-only">KeyLive</span>
-              <h1 className='text-3xl text-indigo-600 font-semibold'>KeyLive</h1>
-              </Link>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt=""
+              />
+            </a>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
@@ -26,18 +32,29 @@ export default function Navbar() {
             </Popover.Button>
           </div>
           <Popover.Group as="nav" className="hidden space-x-10 md:flex">
-
-            <a href="pricing" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a
+              href="pricing"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
               Pricing
             </a>
-            <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+            <a
+              href="#"
+              className="text-base font-medium text-gray-500 hover:text-gray-900"
+            >
               Docs
             </a>
           </Popover.Group>
           <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-            <a href="#" 
-            className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
-            onClick={session ? () => window.location.href = "/dashboard" : () => void signIn()}>
+            <a
+              href="#"
+              className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+              onClick={
+                session
+                  ? () => (window.location.href = "/dashboard")
+                  : () => void signIn()
+              }
+            >
               {session?.user ? session.user.name : "Sign In"}
             </a>
             <a
@@ -60,10 +77,63 @@ export default function Navbar() {
         leaveFrom="opacity-100 scale-100"
         leaveTo="opacity-0 scale-95"
       >
+        <Popover.Panel
+          focus
+          className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
+        >
+          <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+            <div className="px-5 pt-5 pb-6">
+              <div className="flex items-center justify-between">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    alt="KeyLive"
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                    <span className="sr-only">Close menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+            </div>
+            <div className="space-y-6 py-6 px-5">
+              <div className="grid grid-cols-2 gap-y-4 gap-x-8">
+                <a
+                  href="pricing"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  <CurrencyDollarIcon className="h-6 w-6 flex-shrink-0 text-indigo-600" />{" "}
+                  Pricing
+                </a>
+
+                <a
+                  href="features"
+                  className="text-base font-medium text-gray-900 hover:text-gray-700"
+                >
+                  <AdjustmentsHorizontalIcon className="h-6 w-6 flex-shrink-0 text-indigo-600" />{" "}
+                  Features
+                </a>
+              </div>
+              <div>
+                <a
+                  href="#"
+                  className="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                  onClick={
+                    !session
+                      ? () => void signIn()
+                      : () => (window.location.href = "/dashboard")
+                  }
+                >
+                  {!session ? "Sign In" : "Go To Dashboard"}
+                </a>
+              </div>
+            </div>
+          </div>
+        </Popover.Panel>
       </Transition>
     </Popover>
-  )
+  );
 }
-
-
-module.exports = Navbar;
