817a41ba76c8c92d6f6869ab2c0debf7285fc109
Sam <samennis127@gmail.com>
2023-02-02 01:48:34 +0000

    Add triple stats component

---
+++ src/components/TripleStats.tsx
@@ -0,0 +1,49 @@
+import { tripleStatProps } from "../utils/types";
+
+
+export default function TripleStats({ listOfStats }: tripleStatProps) {
+  return (
+    <>
+      <div className="bg-white pt-5 sm:pt-5">
+        <div className="mt-10 bg-white pb-12 sm:pb-16">
+          <div className="relative">
+            <div className="absolute inset-0 h-1/2 bg-white" />
+            <div className="relative mx-auto max-w-7xl px-6 lg:px-8">
+              <div className="mx-auto max-w-4xl">
+                <dl className="rounded-lg bg-white shadow-lg sm:grid sm:grid-cols-3">
+                  <div className="flex flex-col border-b border-gray-100 p-6 text-center sm:border-0 sm:border-r">
+                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+                      {listOfStats[0] ? listOfStats[0].bottomText : ""}
+                    </dt>
+                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">
+                      {listOfStats[0] ? listOfStats[0].topNumber : ""}
+                    </dd>
+                  </div>
+                  <div className="flex flex-col border-t border-b border-gray-100 p-6 text-center sm:border-0 sm:border-l sm:border-r">
+                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+                      {listOfStats[1] ? listOfStats[1].bottomText : ""}
+                    </dt>
+                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">
+                      {listOfStats[1] ? listOfStats[1].topNumber : ""}
+                    </dd>
+                  </div>
+                  <div className="flex flex-col border-t border-gray-100 p-6 text-center sm:border-0 sm:border-l">
+                    <dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+                      {listOfStats[2] ? listOfStats[2].bottomText : ""}
+                    </dt>
+                    <dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">
+                      {listOfStats[2] ? listOfStats[2].topNumber : ""}
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="py-4">
+          <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -2,6 +2,8 @@ import { useSession } from "next-auth/react";
 import type { ReactElement } from "react";
 import Modal from "../components/Modal";
 import SideBar from "../components/Sidebar";
+import TripleStats from "../components/TripleStats";
+import { statisticObject } from "../utils/types";
 import type { NextPageWithLayout } from "./_app";
 
 const Dashboard: NextPageWithLayout = () => {
@@ -10,6 +12,21 @@ const Dashboard: NextPageWithLayout = () => {
   const buttonCallback = () => {
     window.location.href = "/";
   };
+
+  const listofStats: statisticObject[] = [
+    {
+      bottomText: "total product keys",
+      topNumber: "125"
+    },
+    {
+      bottomText: "activated keys",
+      topNumber: "50"
+    },
+    {
+      bottomText: "percentage activated",
+      topNumber: "40%"
+    }
+  ]
 
   return (
     <>
@@ -29,15 +46,12 @@ const Dashboard: NextPageWithLayout = () => {
               <div className="py-6">
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                   <h1 className="text-2xl font-semibold text-gray-900">
-                    Dashboard
+                    Welcome back, {session.user.firstName}
                   </h1>
                 </div>
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                   {/* Replace with your content */}
-
-                  <div className="py-4">
-                    <div className="h-96 rounded-lg border-4 border-dashed border-gray-200" />
-                  </div>
+                  <TripleStats listOfStats={listofStats}/>
                   {/* /End replace */}
                 </div>
               </div>
